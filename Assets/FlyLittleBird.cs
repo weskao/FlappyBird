ca5f845714a9d0af7594b3416ad0d739d409ae3e
Wes <gkm85663@gmail.com>
2020-06-22 16:08:42 +0800

    Bird can fly when click any where in scene now

--- Assets/FlyLittleBird.cs
+++ Assets/FlyLittleBird.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class FlyLittleBird : MonoBehaviour
+namespace Assets
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FlyLittleBird : MonoBehaviour
     {
-        
-    }
+        public float velocity = 1.4f;
+        private Rigidbody2D _rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+            {
+                _rb.velocity = Vector2.up * velocity;
+            }
+        }
     }
 }
