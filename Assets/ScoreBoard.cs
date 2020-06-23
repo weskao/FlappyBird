f4a044eb188be6b37fdb6dc23c60ac4f61129b55
Wes <gkm85663@gmail.com>
2020-06-23 23:52:16 +0800

    Rename

--- Assets/ScoreBoard.cs
+++ Assets/ScoreBoard.cs
@@ -2,21 +2,20 @@
 
 namespace Assets
 {
-    public class AddScore : MonoBehaviour
+    public class ScoreBoard : MonoBehaviour
     {
+        public static int score = 0;
+
         // Start is called before the first frame update
         private void Start()
         {
+            score = 0;
         }
 
         // Update is called once per frame
         private void Update()
         {
-        }
-
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            ScoreBoard.score++;
+            GetComponent<UnityEngine.UI.Text>().text = score.ToString();
         }
     }
 }
