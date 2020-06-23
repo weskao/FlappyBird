4234dc8128e6e0793f3d2d39842b5d1bc77618d0
Wes <gkm85663@gmail.com>
2020-06-23 23:08:46 +0800

    Complete game

---
+++ Assets/AddScore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets
+{
+    public class AddScore : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Score.score ++;
+        }
+    }
+}
